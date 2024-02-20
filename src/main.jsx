032529eb597aef17b0298c416e4305a5f439b241
Megan Grant <granmega@oregonstate.edu>
2024-02-19 16:34:07 -0800

    Getting things going

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Root from '.Root'
 
-import App from './App'
+const queryClient = new QueryClient()
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        children: [
+            { path: "query-search", element: <QuerySearch /> },
+            { path: "query-post", element: <QueryPost /> },
+            {
+                path: "org-repos/:org",
+                element: <RouterOrgRepos />,
+                loader: orgLoader
+            },
+            {
+                path: "router-post",
+                element: <RouterPost />,
+                action: postAction
+            },
+            { index: true, element: <Navigate to="/fetch-search" replace /> }
+        ]
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
     </React.StrictMode>,
 )
